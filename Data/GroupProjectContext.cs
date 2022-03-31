69aba61e0768150d2d7ac24a96974ebdb8b9ff5b
Tyler Gann <tg680253@southeast.edu>
2022-03-31 18:43:10 -0500

    Created Product class/controller and DB Context.

---
+++ Data/GroupProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GroupProject.Models;
+
+namespace GroupProject.Data
+{
+    public class GroupProjectContext : DbContext
+    {
+        public GroupProjectContext (DbContextOptions<GroupProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GroupProject.Models.Product> Product { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ImageName { get; set; }
+        public string Category { get; set; }
+
+    }
+}
