9734b72761b0c714c8451a8113e1468a81005502
theRealjWortmann <jw663080@southeast.edu>
2022-04-07 11:56:35 -0500

    Updated Customer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using GroupProject.Data;
+using GroupProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+            private readonly GroupProjectContext _context;
+           
+
+
+            public CustomerController(GroupProjectContext context)
+            {
+                _context = context;
+                
+            }
+            public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+                Customer newCustomer = new Customer
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Address=model.Address,
+                    Email=model.Email
+                };
+
+                _context.Add(newCustomer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+
+
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,18 @@ namespace GroupProject.Models
     public class Customer
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "You must enter a user name")]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
-        public string FirstName{ get; set; }
+        [Required(ErrorMessage = "You must enter a first name")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "You must enter a last name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "You must enter an address")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "You must enter an email")]
         public string Email { get; set; }
     }
 }
