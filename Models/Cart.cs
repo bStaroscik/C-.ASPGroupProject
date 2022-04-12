cdae6d7ed3efe7968dfede2e8ec51fdcc532dc25
theRealjWortmann <jw663080@southeast.edu>
2022-04-12 17:50:59 -0500

    Cart

---
+++ Models/Cart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupProject.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.Id == product.Id)
+                .FirstOrDefault();
+            //if more than one type of item in cart, add to quantity instead of creating another instance of the same item. 
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += 1;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) => lineCollection.RemoveAll(l => l.Product.Id == product.Id);
+
+        public virtual decimal ComputeTotalValue() => lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+
+        public virtual void Clear() => lineCollection.Clear();
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
+    }
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
