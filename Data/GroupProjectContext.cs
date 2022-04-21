65b03133569a5e02221abe364f0971713b839aaa
Tyler Gann <tg680253@southeast.edu>
2022-04-21 18:54:09 -0500

    Working on Product Edit with Photo.

--- Data/GroupProjectContext.cs
+++ Data/GroupProjectContext.cs
@@ -20,6 +20,7 @@ namespace GroupProject.Data
         public DbSet<Order> Orders {get; set;}
         //public DbSet<Review> Reviews { get; set; }
         public DbSet<Product> Product { get; set; }
+        //public DbSet<ProductViewModel> productViewModels { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
